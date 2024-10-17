b7f8eb3700fbcc126f3d1fac62a3f8814fb58069
liyixin123 <592945148@qq.com>
2024-10-17 09:24:35 +0800

    脚本运行时，获取管理员权限。

--- pack/stop_service.bat
+++ pack/stop_service.bat
@@ -1,4 +1,15 @@
 @echo off
+
+:: Check for administrative privileges
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script requires administrative privileges.
+    echo Please run this script as an administrator.
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~dp0%~nx0' -Verb RunAs"
+    exit /b 1
+)
+
+
 sc stop DWMMonitorService
 if %errorlevel% neq 0 (
     echo Failed to stop DWMMonitorService
